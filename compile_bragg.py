d1b2652e0914aeb135f564db55c4ccb8882cbad2
ziniriccardo <79851673+ziniriccardo@users.noreply.github.com>
2023-02-17 12:18:12 +0100

    Update compile_bragg.py

--- compile_bragg.py
+++ compile_bragg.py
@@ -25,24 +25,19 @@ destination0= config.get('paths', 'stop_powers')
 destination1=config.get('paths','positions')
 destination2=config.get('paths','energies')
 
-
 energies= int(config.get('values','energies'))
 particle=config.get('values','particle')
 material= config.get('values','material')
-
 
 rho= float(config.get('values','rho'))
 Z_over_A= float(config.get('values', 'Z_over_A'))    
 e_ioniz= float(config.get('values', 'e_ioniz'))
 Z=int(config.get('values', 'Z'))
 
-
 mat_properties=[rho,Z_over_A,e_ioniz,Z]
 
-
 #for i in range(0,len(energies)):
- 
-    
+
 plot_vectors=bp.compute_bethe_bloch(particle,energies,material,mat_properties)
    
 #plot_vectors_1=bp.compute_bethe_bloch('pion',100,'carbon')
@@ -50,7 +45,6 @@ plot_vectors=bp.compute_bethe_bloch(particle,energies,material,mat_properties)
 np.save(destination0, plot_vectors[0])
 np.save(destination1,plot_vectors[1])
 np.save(destination2,plot_vectors[2])
-
 
 
 if   len(plot_vectors[0])!=0 and len(plot_vectors[1])!=0 :
