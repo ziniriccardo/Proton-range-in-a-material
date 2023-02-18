d7d4677a6c8d6e105873ed6f17b16c017973efee
ziniriccardo <79851673+ziniriccardo@users.noreply.github.com>
2023-02-18 16:30:11 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,23 +5,26 @@ Created on Mon Feb  6 17:02:22 2023
 @author: ricky
 """
 
-import bragg_peak as bp
+import proton_range as pr
 
-def test_bethe_bloch():
+#@given(energy=0)
+#@settings(max_examples=1)
+
+def test_zero_energy():
     particle='proton'
     material='carbon'
     energy=0
     mat_properties=[ 1.7,0.49954, 78e-6, 6]
-    stop_powers= bp.compute_bethe_bloch(particle, energy, material, mat_properties)
+    stop_powers= pr.compute_range(particle, energy, material, mat_properties)
     assert len(stop_powers[0])==0
   
     
-def test_value_bethe_bloch():
+def test_water_value():
     particle='proton'
     material='water'
     energy=200 
     mat_properties=[ 1.0 ,0.55509, 75e-6, 10]
-    stop_powers= bp.compute_bethe_bloch(particle, energy, material, mat_properties)
+    stop_powers= pr.compute_range(particle, energy, material, mat_properties)
     final_range= stop_powers[1][stop_powers[4]-1]/mat_properties[0]
     aspected_range= 25.8
 
@@ -37,13 +40,14 @@ def test_total_energy():
     material='carbon'
     energy=70
     mat_properties=[ 1.7,0.49954, 78e-6, 6]
-    stop_powers= bp.compute_bethe_bloch(particle, energy, material, mat_properties)
+    stop_powers= pr.compute_range(particle, energy, material, mat_properties)
     
-    total_e=0
+    total_energy=0
     de=0
     
     for i in range(0,len(stop_powers[0])-1):
         de=stop_powers[0][i]*(stop_powers[1][i+1]- stop_powers[1][i])
-        total_e+=de
+        total_energy+=de
     
-    assert total_e<=energy
+    assert total_energy<=energy
+  
